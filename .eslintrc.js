6f4cd1369bee28651413745d1a9342bad9c7ad65
Sergey Sova <i.am@lestad.net>
2017-08-01 14:25:38 +0300

    Add option class-methods-use-this

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,11 +19,23 @@ module.exports = {
     'strict': 'off',
     'no-unused-vars': ['warn', { varsIgnorePattern: 'React', ignoreRestSiblings: false }],
     'import/no-extraneous-dependencies': 'off',
+    "class-methods-use-this": ["warn", { exceptMethods: [
+      "componentDidCatch",
+      "componentDidMount",
+      "componentDidUpdate",
+      "componentWillMount",
+      "componentWillReceiveProps",
+      "componentWillUnmount",
+      "componentWillUpdate",
+      "render",
+      "shouldComponentUpdate",
+    ]}],
 
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
     'react/prop-types': ['warn', {
       ignore: ['classes', 'theme', 'sheet', 'sheets'],
     }],
+
 
     'jsx-a11y/interactive-supports-focus': 'off',
     'jsx-a11y/href-no-hash': 'off'
